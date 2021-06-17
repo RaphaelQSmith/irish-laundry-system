921cbf9cf24a7a0a91768de60bf0752b221af41c
RaphaelQSmith <49982690+RaphaelQSmith@users.noreply.github.com>
2021-06-16 23:37:15 -0300

    Header styling and header buttons

--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -5,12 +5,23 @@ export default function Header() {
     return (
         <div>
             <header>
-                <h2 className={styles.ilsLogo}>Irish Laundry System</h2>
-                <div className="headerInfo">
-                    <p>Email: some@some.com</p>
-                    <p>Phone:555-1212</p>
+                <div className={styles.headerContainer}>
+                    <h2 className={styles.ilsLogo}>Irish Laundry System</h2>
+                    <div className={styles.headerInfo}>
+                        <p>Email: some@some.com</p>
+                        <p>Phone:555-1212</p>
+                    </div>
                 </div>
             </header>
+            <div className={styles.buttonContainer}>
+                <button className={styles.headerBts}>Home</button>
+                <button className={styles.headerBts}>Services</button>
+                <button className={styles.headerBts}>Rental</button>
+                <button className={styles.headerBts}>Equipment</button>
+                <button className={styles.headerBts}>Clients</button>
+                <button className={styles.headerBts}>Blog</button>
+                <button className={styles.headerBts}>About Us</button>
+            </div>
         </div>
     )
 }
