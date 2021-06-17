de765b84831e7733a717124e574908222149b367
RaphaelQSmith <49982690+RaphaelQSmith@users.noreply.github.com>
2021-06-16 22:40:47 -0300

    Layout adjustments

--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import styles from './components.module.css'
 
 export default function Header() {
     return (
         <div>
-            
+            <header>
+                <h2 className={styles.ilsLogo}>Irish Laundry System</h2>
+                <div className="headerInfo">
+                    <p>Email: some@some.com</p>
+                    <p>Phone:555-1212</p>
+                </div>
+            </header>
         </div>
     )
 }
